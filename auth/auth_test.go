2f918126aadcfedff052667e676d35157c97c8e7
uptutu <alexkung@outlook.com>
2022-03-01 14:41:01 +0800

    auth fix

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,16 +1,14 @@
 package auth
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAuthenticate(t *testing.T) {
-	token := "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0a2VlbCIsImV4cCI6MTY0NjEyMTk0Nywic3ViIjoidXNyLTIzNDgwMmM5YWQwY2NjOGUxYTViYWQ0NWZiNmMifQ.EUvVpq_ITnTZgJhH1KlUIzPReCU-IUnFN5FritWn2Co3GRhvTVDboR2xM7J4T2EIjI-5eq3dZrGOsSudx86_sA"
+	token := "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0a2VlbCIsImV4cCI6MTY0NjEyMzQ2NSwic3ViIjoidXNyLTY1OTg3MGMzZTY5OTNlODgxMWMxOGRhNmM2YWEifQ.A3vgIXU2exn66uhDb3ANrtVBiz9yrn-y18f9HisaIhAUy208FkgFUmHtTdbZ3rgEDanRuaoBtwNGBkaA3ZOFXA"
 	auth, err := Authenticate(token)
 	assert.NoError(t, err)
-	fmt.Printf("%+v\n", auth)
 	assert.NotEmpty(t, auth)
 }
