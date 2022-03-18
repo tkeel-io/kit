a2d4b547e0c1b3afd9db02384eb38f53857448ae
uptutu <alexkung@outlook.com>
2022-03-18 14:25:17 +0800

    auth tool for get user info form context XtKeelAuth

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestAuthenticate(t *testing.T) {
 	token := "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0a2VlbCIsImV4cCI6MTY0NjE0MDI5Miwic3ViIjoidXNyLTIzNDgwMmM5YWQwY2NjOGUxYTViYWQ0NWZiNmMifQ.n3xo5lavvWz5tBV-Gs0UPFafP69Aumfn2L38DTm_E_VVUhLG7SblTBZqgtlyjHfD5qVmJH8iIsJmy-hkAWYz4w"
-	auth, err := Authenticate(token, AuthTokenURLTestRemote)
+	auth, err := Authenticate(token, TestRemote)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, auth)
 }
