f8f443645db49ddbf0193ab55a9ebb30bf31f239
柳丰 <grantliu@yunify.com>
2022-05-10 21:47:17 +0800

    feat: add StructValue Collect encode

---
+++ encoding/tdtl.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2021 The tKeel Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package encoding
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tkeel-io/tdtl"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+func NewStructValue(cc *tdtl.Collect) *structpb.Value {
+	switch cc.Type() {
+	case tdtl.Bool:
+		ret := cc.To(tdtl.Bool)
+		switch ret := ret.(type) {
+		case tdtl.BoolNode:
+			return structpb.NewBoolValue(bool(ret))
+		}
+	case tdtl.Int, tdtl.Float, tdtl.Number:
+		ret := cc.To(tdtl.Number)
+		switch ret := ret.(type) {
+		case tdtl.IntNode:
+			return structpb.NewNumberValue(float64(ret))
+		case tdtl.FloatNode:
+			return structpb.NewNumberValue(float64(ret))
+		}
+	case tdtl.String:
+		return structpb.NewStringValue(cc.String())
+	case tdtl.JSON, tdtl.Object, tdtl.Array:
+		ret := &structpb.Struct{}
+		err := json.Unmarshal(cc.Raw(), ret)
+		if err != nil {
+			fmt.Println("err", err)
+		}
+		return structpb.NewStructValue(ret)
+	}
+	return structpb.NewBoolValue(false)
+}
+
+func NewCollectValue(val *structpb.Value) (*tdtl.Collect, error) {
+	byt, err := json.Marshal(val)
+	if err != nil {
+		return nil, err
+	}
+	ret := tdtl.New(byt)
+	return ret, nil
+}
--- go.mod
+++ go.mod
@@ -22,9 +22,16 @@ require (
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
-require github.com/pkg/errors v0.8.1
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/tkeel-io/tdtl v0.1.4
+)
 
 require (
+	github.com/antlr/antlr4 v0.0.0-20200503195918-621b933c7a7f // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 )
